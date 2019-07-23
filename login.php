87ae82eb7b4bae2b059491f4fc67c4f30419f531
Tushar Sharma <imtusharsharma@gmail.com>
2019-07-24 00:26:20 +0530

    Update login.php

--- login.php
+++ login.php
@@ -15,7 +15,7 @@
         $myusername = mysqli_real_escape_string($db,$_POST['username']);
         $mypassword = mysqli_real_escape_string($db,$_POST['password']);
 
-        $sql = "SELECT id, username FROM Test_Table WHERE username = '$myusername' and password = '$mypassword'";
+        $sql = "SELECT id, username FROM credentials WHERE username = '$myusername' and password = '$mypassword'";
         $result = mysqli_query($db,$sql);
         $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
 
